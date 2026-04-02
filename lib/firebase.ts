3d3e5fd71c9499110c38b1ca01e6777724f74ef3
NTWKKM <NTWKKM@users.noreply.github.com>
2026-04-02 19:46:52 +0700

    patch

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, enableMultiTabIndexedDbPersistence } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPe0VuVAtIjQMbOLmMu2hht2yGZfww7hE",
-  authDomain: "er-mnrh-consultingform.firebaseapp.com",
-  projectId: "er-mnrh-consultingform",
-  storageBucket: "er-mnrh-consultingform.firebasestorage.app",
-  messagingSenderId: "759379378215",
-  appId: "1:759379378215:web:5f26209516b14a68080e02",
-  measurementId: "G-QQG87YV85F"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase only if it hasn't been initialized already (important for Next.js hot-reloading)
